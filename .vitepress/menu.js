92e1dad166654ed4acdd408b0898eab783a4e2d2
kxfang2 <kxfang2@iflytek.com>
2025-07-01 20:58:31 +0800

    🚗 麒麟会怎么运作

--- .vitepress/menu.js
+++ .vitepress/menu.js
@@ -212,6 +212,7 @@ export function sidebar() {
                     {text: "少妇战法训练课", link: "04.md"},
                     {text: "少妇战法回顾总结", link: "05.md"},
                     {text: "补票战法", link: "06.md"},
+                    {text: "麒麟会如何运作", link: "07.md"},
                 ],
                 collapsed: true,
             }
